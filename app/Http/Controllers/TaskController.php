b78c3697aa7f8caae018d5a537fcbbb316cd17d9
Leroy6376 <roza4466@mail.ru>
2025-04-02 13:29:36 +0300

    Functionality implementation

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\TaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(TaskRequest $request)
+    {
+        $data = $request->validated();
+        $search = $data['search'] ?? '';
+
+        if (isset($data['sort'])) {
+            $tasks = TaskResource::collection(Task::query()->whereLike('title', "%$search%")->
+            orderBy($data['sort'], 'desc')->paginate($data['per_page'])->withQueryString());
+        } else {
+            $tasks = TaskResource::collection(Task::query()->whereLike('title', "%$search%")->
+            paginate($data['per_page'])->withQueryString());
+        }
+        return $tasks;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $data = $request->validated();
+        $task = Task::firstOrCreate($data);
+        return new TaskResource($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+        return new TaskResource($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json(["message" => "Task deleted successfully"]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::Apiresource('tasks', \App\Http\Controllers\TaskController::class);
